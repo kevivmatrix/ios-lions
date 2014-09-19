8cf0017d78f7bbe0686da4733f96c76a7902e12a
Keviv vivek <vivekpatna52@gmail.com>
2014-09-19 12:22:29 +0530

    basic setup done

---
+++ lions/Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  lions
+//
+//  Created by vivek on 19/09/14.
+//  Copyright (c) 2014 vivek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age:Int = 0
+    var name:String = ""
+    var breed:String = ""
+    var image:UIImage = UIImage(named: "")
+}
+
--- lions/ViewController.swift
+++ lions/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    @IBAction func nextButtonClicked(sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var firstTiger = Tiger(age: 3, name: "tigger", breed: "bengal", image: UIImage(named: "BengalTiger.jpg"))
+        var secondTiger = Tiger(age: 5, name: "tigress", breed: "indo chinese", image: UIImage(named: "IndochineseTiger.jpg"))
+        var thirdTiger = Tiger(age: 4, name: "jacob", breed: "siberian", image: UIImage(named: "SiberianTiger.jpg"))
+        var fourthTiger = Tiger(age: 3, name: "spar", breed: "malayan", image: UIImage(named: "MalayanTiger.jpg"))
+        
+        var tigers = [ firstTiger, secondTiger, thirdTiger, fourthTiger ]
+        
+        imageView.image = firstTiger.image
+        nameLabel.text = firstTiger.name
+        ageLabel.text = "\(firstTiger.age)"
+        breedLabel.text = firstTiger.breed
     }
 
     override func didReceiveMemoryWarning() {
